a324619c9f052fa80b65bb68263f28ec07d93df5
Deniss <doxideniss@gmail.com>
2020-03-06 22:47:13 +0200

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return Array.isArray(array) && array.length ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return Array.isArray(array) && array.length ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return Array.isArray(array) && array.length ? (
+        array.reduce((t, n) => t + n, 0) / array.length
+    ) : 0;
 }
